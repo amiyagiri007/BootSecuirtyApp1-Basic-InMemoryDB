d036df71aff1ec46bb3ce55ac38de8d60ba8e04c
amiyagiri007 <amiyakrushnagiri430@gmail.com>
2025-03-31 00:41:40 +0530

    home.jsp added and API created.

--- src/main/java/com/example/bootsecuirtyapp1basicinmemorydb/controller/BankOperationController.java
+++ src/main/java/com/example/bootsecuirtyapp1basicinmemorydb/controller/BankOperationController.java
@@ -1,4 +1,30 @@
 package com.example.bootsecuirtyapp1basicinmemorydb.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BankOperationController {
+    @GetMapping("/")
+    public String showHome(){
+return "home";
+    }
+
+    @GetMapping("/offers")
+    public String showOffers(){
+        return "offers";
+    }
+    @GetMapping("/balance")
+    public String showBalance(){
+        return "show_balance";
+    }
+    @GetMapping("/approveLoan")
+    public String loans(){
+        return "loan";
+    }
+    //for authorization failed
+    @GetMapping("/denied")
+    public String denied(){
+        return "denied";
+    }
 }
---
+++ src/main/webapp/WEB-INF/pages/home.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ASUS
+  Date: 31-03-2025
+  Time: 00:36
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+<h1 style="color: red;text-align: center">Home Page</h1>
+<br>
+<br><br>
+</body>
+</html>
